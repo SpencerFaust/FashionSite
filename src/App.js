0862fb9651d102dfcffeb13248d9803b0098805a
Spencer Faust <spencer.faust@tempworks.com>
2019-11-26 13:34:22 -0600

    "Added menu button and animation"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import NavIcon from "../src/NavMenu/NavIcon";
+import NavigationMenu from "./NavMenu/NavigationMenu";
 
 function App() {
+  const [navOpen, setNavOpen] = useState(false);
+  const handleNavToggle = () => {
+    console.log("NAV", navOpen);
+    setNavOpen(!navOpen);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationMenu open={navOpen} />
+      <NavIcon buttonFunction={handleNavToggle} />
     </div>
   );
 }
